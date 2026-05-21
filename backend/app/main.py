d8c4ee924c9387a99c02bce2585420a65b5087de
emoralesm37 <emoralesm37@miumg.edu.gt>
2026-05-21 00:33:56 -0600

    Agrega ruta del Mundial a la aplicacion FastAPI M5
    Agrega ruta del Mundial a la aplicacion FastAPI M5

--- backend/app/main.py
+++ backend/app/main.py
@@ -5,6 +5,8 @@ from app.api.controllers.auth_controller import router as auth_router
 from app.api.controllers.invitacion_controller import router as invitacion_router
 from app.api.controllers.usuario_controller import router as usuario_router
 
+from app.modules.mundial.router import mundial_router
+
 app = FastAPI(
     title="Liga Mundial API",
     version="1.0.0"
@@ -27,6 +29,7 @@ app.include_router(auth_router)
 app.include_router(invitacion_router)
 app.include_router(usuario_router)
 
+app.include_router(mundial_router)
 
 @app.get("/")
 def root():
